fded0b69775f3b0b8b3950bbe5b389cc2ae0db15
David-Kasilia <davidkasilia001@gmail.com>
2022-11-24 14:52:57 +0300

    Completed methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
     belongs_to :author, class_name: 'User'
     belongs_to :posts, class_name: 'Post'
+    
+    # A method that updates the comments counter for a post.
+    def updates_comments_counter_post
+        posts.increment!(:comments_counter)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_many :comments
     has_many :likes
+
+    # A method that updates the posts counter for a user.
+    public    
+    def updates_post_counter_user
+        author.increment!(:post_counter)
+    end
+
+    # A method which returns the 5 most recent comments for a given post.
+    public
+    def most_recent_comments
+        Comment.order(created_at: :desc).limit(5)
+    end
 end
