86d55c091cb42b327a9b6b0973f0539957517469
David-Kasilia <davidkasilia001@gmail.com>
2022-11-29 01:48:52 +0300

    Add Test cases for posts

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe PostsController do
+    describe "GET index" do
+        it "has a 200 status code" do
+            get :index, params: {user_id: 123, param: :user_id}
+            expect(response.status).to eq(200)
+        end
+        it "has a 200 status code" do
+            get :index, params: {user_id: 123, param: :user_id}
+            expect(response).to render_template("posts")
+        end
+    end
+end
