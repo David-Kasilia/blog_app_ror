144a19af1e2a22c6a148cdc5736ccf53f558b856
David-Kasilia <davidkasilia001@gmail.com>
2022-11-29 01:32:36 +0300

    Complete tests for users controller

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+    describe "GET index" do
+        it "has a 200 status code" do
+            get :index
+            expect(response.status).to eq(200)
+        end
+        it "renders the index template" do
+            get :index
+            expect(response).to render_template("users")
+        end
+    end
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe 'Users', type: :request do
       get '/users/:id'
       expect(response).to have_http_status(:success)
     end
+    it "response body includes correct placeholder text." do
+      get '/users'
+      expect(response.body).to include("Display The List Of Users")
+  end
+  it 'response body includes correct placeholder text.' do
+    get '/users/:id'
+    expect(response.body).to include("Display The User Details")
+  end
   end
 end
