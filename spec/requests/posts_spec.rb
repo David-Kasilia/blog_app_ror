fad65d02a47d74981ef47493706dc5299e95cf24
David-Kasilia <davidkasilia001@gmail.com>
2022-11-29 00:21:41 +0300

    Created unit test for requests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /posts' do
     it 'returns http success' do
-      get '/posts/posts'
+      get '/users/:user_id/posts'
+      expect(response).to have_http_status(:success)
+    end
+    it 'returns http success' do
+      get '/users/:user_id/posts/:id'
       expect(response).to have_http_status(:success)
     end
   end
