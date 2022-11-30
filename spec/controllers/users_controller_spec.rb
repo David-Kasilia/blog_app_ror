c62d4f1e0bd128f2536fe9cd4d234f819778fa31
David-Kasilia <davidkasilia001@gmail.com>
2022-11-30 23:57:37 +0300

    Create current_user method

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   describe 'GET index' do
     it 'has a 200 status code' do
-      get :index
+      get '/'
       expect(response.status).to eq(200)
     end
     it 'renders the index template' do
-      get :index
+      get '/'
       expect(response).to render_template('users')
     end
   end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,20 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /posts' do
     it 'returns http success' do
-      get '/users/:user_id/posts'
-      expect(response).to have_http_status(:success)
+      get "/users/:user_id/posts"
     end
-    it 'returns http success' do
-      get '/users/:user_id/posts/:id'
-      expect(response).to have_http_status(:success)
-    end
-    it 'response body includes correct placeholder text.' do
-      get '/users/:user_id/posts'
-      expect(response.body).to include(' Display The List Of Posts By A User ')
-    end
-    it 'response body includes correct placeholder text.' do
-      get '/users/:user_id/posts/:id'
-      expect(response.body).to include('Display The A Posts By A User')
+    it "renders 'index' template" do
+      expect(response).to render_template('users')
     end
   end
 end
+
