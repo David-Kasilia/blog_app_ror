40cced90cfae9dbc93ed9b2e785e1324420f0cc1
David-Kasilia <davidkasilia001@gmail.com>
2022-12-01 12:55:27 +0300

    Create form for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,23 @@ class CommentsController < ApplicationController
   def new
     @comments = Comment.new
   end
+
+  def create
+    @comment = Comment.new(comments_params)
+    @post = Post.find_by(id: params[:post_id])
+    @comment.author_id = current_user.id
+    @comment.post_id = @post.id
+
+    if @comment.save
+      redirect_to user_post_path(user_id: @post.author_id, id: @post.id)
+    else
+      render :new, alert: ':( Cannot Create comment retry again :('
+    end
+  end
+
+  private
+
+  def comments_params
+    params.require(:comments).permit(:text)
+  end
 end
