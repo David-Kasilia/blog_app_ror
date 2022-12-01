62fed6730ad8cdefc6c3682f91fd091b34262b2a
David-Kasilia <davidkasilia001@gmail.com>
2022-12-01 12:43:58 +0300

    Create create mehod for post submission

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,21 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @author = User.find_by(id: params[:user_id])
+    @post = @author.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_path(id :@post.author_id)
+    else
+      render :new, alert: ':( Cannot Create post retry again :('
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
